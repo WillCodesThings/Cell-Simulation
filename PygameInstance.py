5ac0bed7eded00f359cd00568e2a8782c5df6518
WillCodesThings <95990125+WillCodesThings@users.noreply.github.com>
2023-05-21 21:53:00 -0700

    Update PygameInstance.py
    Better implementation, as well as accounting for cell collision

--- PygameInstance.py
+++ PygameInstance.py
@@ -1,138 +1,98 @@
 import pygame
 import random
-
-# GLOBAL VARIABLES
-COLOR = (255, 100, 98)
-SURFACE_COLOR = (167, 255, 100)
-WIDTH = 500
-HEIGHT = 500
+from brain import NNet
+# Define the Cell class
 
 
-class NN:
-    def __init__(self, nodes):
-        self.nodes = nodes
-
-    def feedForeward(self):
-        return self.nodes.activate()
-
-
-class moveF:
-    def __init__(self, hidden):
-        self.hidden = hidden
-
-    def activate(self):
-        return self.hidden.activate() * 0.7 + .2
-
-
-class hidden:
-    def __init__(self, firstNode):
-        self.firstNode = firstNode
-
-    def activate(self):
-        return self.firstNode.activate() * 0.8 + 0.02
-
-
-class inputNode:
-    def __init__(self):
-        self.weights = 0.2
-        self.bias = 0.1
-
-    def activate(self):
-        return self.weights * self.weights + self.bias
-
-# Object class
-
-
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, color, height, width, cellid):
+class Cell(pygame.sprite.Sprite):
+    def __init__(self, x, y, size, color, width, height, speed):
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(SURFACE_COLOR)
-        self.image.set_colorkey(COLOR)
-        self.cellid = cellid
-
-        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
-
+        self.image = pygame.Surface((size, size))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = speed
+        self.canvas_width = width
+        self.canvas_height = height
+
+    def update(self, cells):
+        # Move the cell randomly
+
+        # Ensure the cell stays within the canvas boundaries
+        self.rect.x = max(
+            0, min(self.rect.x, self.canvas_width - self.rect.width))
+        self.rect.y = max(
+            0, min(self.rect.y, self.canvas_height - self.rect.height))
+
+        # Check for collisions with other cells
+        for cell in cells:
+            if cell != self and self.rect.colliderect(cell.rect):
+                dx = self.rect.x - cell.rect.x
+                dy = self.rect.y - cell.rect.y
+                distance = (dx ** 2 + dy ** 2) ** 0.5
+                min_distance = (self.rect.width + cell.rect.width) / 2
+
+                if distance != 0 and distance < min_distance:
+                    overlap = min_distance - distance
+                    overlap_x = overlap * dx / distance
+                    overlap_y = overlap * dy / distance
+
+                    self.rect.x += int(overlap_x / 2)
+                    self.rect.y += int(overlap_y / 2)
+                    cell.rect.x -= int(overlap_x / 2)
+                    cell.rect.y -= int(overlap_y / 2)
 
 
+# Initialize Pygame
 pygame.init()
 
-RED = (255, 0, 0)
+# Set up the canvas
+width, height = 800, 600
+canvas = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Random Cell Movement")
 
-size = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Creating Sprite")
+# Create a group for all sprites
+all_sprites = pygame.sprite.Group()
 
-all_sprites_list = pygame.sprite.Group()
+# Create the cells
+num_cells = 10
+cell_size = 20
+cell_color = (255, 0, 0)  # Red
 
+cells = []
+for _ in range(num_cells):
+    x = random.randint(0, width - cell_size)
+    y = random.randint(0, height - cell_size)
+    speed = random.randint(1, 5)
+    cell = Cell(x, y, cell_size, cell_color, width, height, speed)
+    cells.append(cell)
+    all_sprites.add(cell)
 
-for i in range(10):
-    object_ = Sprite((random.randint(0, 255), random.randint(
-        0, 255), random.randint(0, 255)), 10, 10, i + 1)
-    object_.rect.x = random.randint(10, 490)
-    object_.rect.y = random.randint(10, 490)
-    # if object_.cellid == 2:
-    #     object_.rect.x = 100
-    # else:
-    #     object_.rect.x = 400
-    all_sprites_list.add(object_)
-
-
-cell1NN = NN(moveF(hidden(inputNode())))
-
-
-exit = True
+# Set up the clock
 clock = pygame.time.Clock()
 
-while exit:
+# Main game loop
+running = True
+while running:
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit = False
+            running = False
 
-    print(cell1NN.feedForeward())
-    nextToUse = []
-    for i in all_sprites_list:
+    # Update all sprites
+    all_sprites.update(cells)
 
-        cellIDToXY = [(i.cellid) for i in all_sprites_list]
-        XandYs = [(i.rect.x, i.rect.y) for i in all_sprites_list]
-        del XandYs[cellIDToXY.index(i.cellid)]
-        if cell1NN.feedForeward() >= 0.24:
-            # stop at edge commands (might need to ajust)
-            for h in XandYs:
-                print("hi")
-                print(XandYs)
-                print([(i.rect.x, i.rect.y)])
-                if ((i.rect.x == h[0]) and (i.rect.y == h[1])):
-                    # i.rect.y -= 20
-                    nextToUse.append((i, i.cellid))
-                    print("why")
-                    
-                elif (((i.rect.x + 10 <= h[0]) and (i.rect.y + 10 <= h[1])) or ((i.rect.x + 10 <= h[0]) and (i.rect.y - 10 >= h[1])) or ((i.rect.x - 10 >= h[0]) and (i.rect.y + 10 <= h[1])) or ((i.rect.x - 10 >= h[0]) and (i.rect.y - 10 >= h[1]))):
-                    # i.rect.y -= 20
-                    nextToUse.append((i, i.cellid))
-                    print("whyt")
-            for h in nextToUse:
-                if h[1] == 1:
-                    h[0].rect.x += 10
-                else: 
-                    h[0].rect.x -= 10
-            if (i.rect.x >= (WIDTH)):
-                i.rect.x -= 10
-            elif (i.rect.x <= 0):
-                i.rect.x += 10
-            elif (i.rect.y <= 0):
-                i.rect.y += 10
-            elif (i.rect.y >= (HEIGHT)):
-                i.rect.y -= 20
-                i.rect.x -= 20
-            else:
-                if i.cellid == 1:
-                    i.rect.x -= 1
-                if i.cellid == 2:
-                    i.rect.x += 1
-    all_sprites_list.update()
-    screen.fill(SURFACE_COLOR)
-    all_sprites_list.draw(screen)
+    # Clear the canvas
+    canvas.fill((255, 255, 255))  # White
+
+    # Draw all sprites
+    all_sprites.draw(canvas)
+
+    # Update the display
     pygame.display.flip()
-    clock.tick(90)
+
+    # Limit the frame rate
+    clock.tick(30)
+
+# Quit the game
+pygame.quit()
